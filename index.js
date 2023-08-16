31f343e29dca3444636d9e95abec285dbec936db
su8ject <krivizyuk.andrei@gmail.com>
2023-08-17 01:43:55 +0300

    deleted log

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const giveClass = (className) => {
     document.querySelector("html").classList.add(className);
 };
 
-console.log(1 < 1);
-
 if (1 < month && month < 5) {
     giveClass("spring");
 } else if (4 < month && month < 8) {
